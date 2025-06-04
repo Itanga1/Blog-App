61b1cf1d1ed3820161ec00e1e43ac230f1bae5e2
itanga benigne <itgbenny@gmail.com>
2025-06-04 20:56:32 +0200

    Changed the size of the not-found component

--- app/not-found.js
+++ app/not-found.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const notFound = () => {
   return ( 
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center h-[calc(100vh-170px)]">
       <h1 className="text-8xl text-center text-red-600">404</h1>
       <h1 className="text-4xl font-bold text-center">Page Not Found</h1>
       <p className="text-center text-xl">The page you are looking for does not exist.</p>
