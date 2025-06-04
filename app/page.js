9dc4b4e9edc247492a86a692f4db1e1e2217a699
itanga benigne <itgbenny@gmail.com>
2025-06-04 07:35:17 +0200

    fetched posts from an api and isplayed them on the home/posts page

--- app/page.js
+++ app/page.js
@@ -1,34 +1,32 @@
 import Link from "next/link";
+import createConnection from "@/lib/db";
+import Axios from "axios";
 
-export default function Home() {
+
+export default async function Home() {
+  let posts = [];
+  try{
+    const response = await Axios.get(`${process.env.SERVER_URL}/api/posts`);
+    posts = response.data;
+  }catch (error) {
+    console.error("Error fetching posts:", error);
+  }
+
   return (
     <div className="Home">
       <h1 className="text-3xl font-bold text-center">Blog posts</h1>
       <div className="flex flex-col items-center justify-center mt-5 gap-3">
-        <div className="flex flex-col p-2 bg-blue-100 rounded-lg shadow-md gap-2 max-w-5/7">
-          <p>
-            fhhhhhhhhhh hjgjgjg jgj jhkh khhkh k khkhk hhkkkkkkkkk hkhkh kkkkkkkkhkh hkhkkkkkk k hkkkkkkkkkkkj jkjk kjk j kjk jkjkjjkjkjh g  gjjhkhkh jhkjkjjkyhgjj j jjhkjkh hkhkkh hhhkhk hkhhh jhhkhkhg j ghkhkhghih    ihihi h g hihihihih ik j kggug
-          </p>
-          <Link href={'/blog'} className="self-end pr-2 text-blue-500 underline">View more</Link>
-        </div>
-        <div className="flex flex-col p-2 bg-blue-100 rounded-lg shadow-md gap-2 max-w-5/7">
-          <p>
-            fhhhhhhhhhh hjgjgjg jgj jhkh khhkh k khkhk hhkkkkkkkkk hkhkh kkkkkkkkhkh hkhkkkkkk k hkkkkkkkkkkkj jkjk kjk j kjk jkjkjjkjkjh g  gjjhkhkh jhkjkjjkyhgjj j jjhkjkh hkhkkh hhhkhk hkhhh jhhkhkhg j ghkhkhghih    ihihi h g hihihihih ik j kggug
-          </p>
-          <Link href={'/blog'} className="self-end pr-2 text-blue-500 underline">View more</Link>
-        </div>
-        <div className="flex flex-col p-2 bg-blue-100 rounded-lg shadow-md gap-2 max-w-5/7">
-          <p>
-            fhhhhhhhhhh hjgjgjg jgj jhkh khhkh k khkhk hhkkkkkkkkk hkhkh kkkkkkkkhkh hkhkkkkkk k hkkkkkkkkkkkj jkjk kjk j kjk jkjkjjkjkjh g  gjjhkhkh jhkjkjjkyhgjj j jjhkjkh hkhkkh hhhkhk hkhhh jhhkhkhg j ghkhkhghih    ihihi h g hihihihih ik j kggug
-          </p>
-          <Link href={'/blog'} className="self-end pr-2 text-blue-500 underline">View more</Link>
-        </div>
-        <div className="flex flex-col p-2 bg-blue-100 rounded-lg shadow-md gap-2 max-w-5/7">
-          <p>
-            fhhhhhhhhhh hjgjgjg jgj jhkh khhkh k khkhk hhkkkkkkkkk hkhkh kkkkkkkkhkh hkhkkkkkk k hkkkkkkkkkkkj jkjk kjk j kjk jkjkjjkjkjh g  gjjhkhkh jhkjkjjkyhgjj j jjhkjkh hkhkkh hhhkhk hkhhh jhhkhkhg j ghkhkhghih    ihihi h g hihihihih ik j kggug
-          </p>
-          <Link href={'/blog'} className="self-end pr-2 text-blue-500 underline">View more</Link>
-        </div>
+        {
+          posts.map((post)=>{
+            return(
+              <div className="flex flex-col p-2 bg-blue-100 rounded-lg shadow-md gap-2 max-w-5/7" key={post.id}>
+                <h2 className="text-2xl font-semibold">{post.title}</h2>
+                <p>{post.description}</p>
+                <Link href={'/blog'} className="self-end pr-2 text-blue-500 underline">View more</Link>
+              </div>
+            )
+          })
+        }
       </div>
     </div>
   );
