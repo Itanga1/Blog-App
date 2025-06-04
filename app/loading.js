e03d8ae53e7f02e40a1f9f9623d528a11e5267bc
itanga benigne <itgbenny@gmail.com>
2025-06-04 09:14:48 +0200

    Created a more stylish loader

--- app/loading.js
+++ app/loading.js
@@ -1,7 +1,8 @@
 export default function Loading(){
   return ( 
-    <div>
-      <h1>Loading....</h1>
+    <div role="status" className=" flex flex-col items-center justify-center gap-3 h-[calc(100vh-90px)]">
+      {/* From Uiverse.io by Fresnel11 */}
+      <div className="w-12 h-12 border-4 border-t-blue-500 border-gray-300 rounded-full animate-spin"></div>
     </div>
    );
 }
