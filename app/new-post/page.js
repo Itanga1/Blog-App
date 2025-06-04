38a62ddf7e73d4ef09c8c05c0a1e999ca611dc57
itanga benigne <itgbenny@gmail.com>
2025-06-04 09:16:45 +0200

    created the add new post function

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,13 +1,40 @@
+"use client";
+import Axios from "axios";
+import { useState} from "react";
 export default function NewBlog(){
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const createPost = (e)=>{
+    e.preventDefault();
+    console.log(process.env.NEXT_PUBLIC_SERVER_URL)
+    try{
+      Axios.post(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/createPost`, {
+        title,
+        description
+      }).then((response)=>{
+        if(response.data.message){
+          console.log(response.data.message);
+          setTitle("");
+          setDescription("");
+        }else{
+          console.error(response.data.error);
+        }
+      })
+    }catch (error) {
+      console.error("Error creating post:", error);
+    }
+  }
+
   return(
     <div className=" flex flex-col items-center justify-center gap-3 h-[calc(100vh-90px)]">
       <h1 className="text-3xl font-bold text-center">Add New Post</h1>
-      <form className=" flex flex-col shadow-lg p-10 mt-2 justify-evenly min-h-[400px] w-[600px]" >
+      <form className=" flex flex-col shadow-lg p-10 mt-2 justify-evenly min-h-[400px] w-[600px]" onSubmit={createPost}>
         <label>Post title: </label>
-        <input type="text" className=" border rounded-lg py-[10px] px-[20px]"/>
+        <input value={title} type="text" className=" border rounded-lg py-[10px] px-[20px]" onChange={(e)=>setTitle(e.target.value)} required/>
         <label>Description:</label>
-        <textarea name="" id="" placeholder="Type post description" className="border rounded-lg py-[10px] px-[20px] h-[80px] resize-none" ></textarea>
-        <button className=" text-white bg-blue-500 rounded-lg px-20 py-3 cursor-pointer font-bold">Create Post</button>
+        <textarea value={description} name="" id="" placeholder="Type post description" className="border rounded-lg py-[10px] px-[20px] h-[80px] resize-none" onChange={(e)=>setDescription(e.target.value)} minLength={150} required></textarea>
+        <button className=" text-white bg-blue-500 rounded-lg px-20 py-3 cursor-pointer font-bold" type="submit">Create Post</button>
       </form>
     </div>
   )
