3509df2ce7cd59033f8c787e5427bbdd899222ed
itanga benigne <itgbenny@gmail.com>
2025-06-04 20:55:16 +0200

    added the update and delete buttons in the post/post review page

--- app/post/[id]/page.js
+++ app/post/[id]/page.js
@@ -4,13 +4,16 @@ export default async function viewPost({ params }) {
   const { id } = await params;
   const response = await fetch(`${process.env.SERVER_URL}/api/posts/${id}`);
   const post = await response.json();
-  console.log("Post data:", post);
   return (
-    <div className="flex flex-col items-center justify-center mt-5 gap-3">
+    <div className="flex flex-col items-center justify-center mt-5 gap-3 min-h-[calc(50vh-90px)]">
       <h1 className="text-3xl font-bold text-center">Post Details</h1>
-      <div className="flex flex-col p-2 bg-blue-100 rounded-lg shadow-md gap-2 w-5/7">
+      <div className="flex flex-col bg-blue-100 rounded-lg shadow-md gap-2 w-5/7 p-5">
         <h2 className="text-2xl font-semibold">{post.title}</h2>
         <p>{post.description}</p>
+        <div className=' flex flex-row justify-end gap-2 mt-2'>
+          <button className=' py-[10px] px-[20] font-bold text-md text-white rounded-2xl bg-blue-500'>Update</button>
+          <button className=' py-[10px] px-[20] font-bold text-md text-white rounded-2xl bg-red-500'>Delete</button>
+        </div>
       </div>
     </div>
   )
