6c46e13ac995f799c8ebb5c938c82bbb060d031a
itanga benigne <itgbenny@gmail.com>
2025-06-04 20:55:49 +0200

    Changed the height of the loading component

--- app/loading.js
+++ app/loading.js
@@ -1,6 +1,6 @@
 export default function Loading(){
   return ( 
-    <div role="status" className=" flex flex-col items-center justify-center gap-3 h-[calc(100vh-90px)]">
+    <div role="status" className=" flex flex-col items-center justify-center gap-3 h-[calc(100vh-170px)]">
       {/* From Uiverse.io by Fresnel11 */}
       <div className="w-12 h-12 border-4 border-t-blue-500 border-gray-300 rounded-full animate-spin"></div>
     </div>
