4b14bb206e0a370952de360f50745d52b20d155b
itanga benigne <itgbenny@gmail.com>
2025-06-06 19:36:18 +0200

    modified the delete post route codes to more efficient ones

--- app/api/deletePost/[id]/route.js
+++ app/api/deletePost/[id]/route.js
@@ -1,13 +1,21 @@
 import connection from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export default async function DELETE({params}){
-  const { id } = await params;
+export async function DELETE(request, {params}){
+  const { id } = params;
   const sql = "DELETE FROM blogs WHERE id = ?";
-  const values = [id];
   try{
-    await connection.query(sql, values);
-    return NextResponse.json({message: "Post deleted successfully"}, {status: 200});
+    const [checkResult] = await connection.query(
+      "SELECT id FROM blogs WHERE id = ?", 
+      [id]
+    );
+    if (checkResult.length === 0) {
+      return NextResponse.json({error: "Post not found"}, {status: 404});
+    }else{
+      await connection.query(sql, [id]);
+      return NextResponse.json({message: "Post deleted successfully"}, {status: 200});
+    }
+    
   }catch (error) {
     console.error("Error deleting post:", error);
     return NextResponse.json({error: "Failed to delete post"}, {status: 500});
