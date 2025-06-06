d03cc0b8628b4c707ad8644f46d346505986d06a
itanga benigne <itgbenny@gmail.com>
2025-06-06 19:37:40 +0200

    modified the codes so that posts are fetched directly from the database instead from the api

--- app/page.js
+++ app/page.js
@@ -1,22 +1,20 @@
 import Link from "next/link";
-import Axios from "axios";
+import connection from "@/lib/db";
+
+export const dynamic = "force-dynamic";// this line ensures that the page is always revalidated and does not use static generation
+// This is important for dynamic data fetching in Next.js 13+ with the app directory
 
 
 export default async function Home() {
-  let posts = [];
-  try{
-    const response = await Axios.get(`${process.env.SERVER_URL}/api/posts`);
-    posts = response.data;
-  }catch (error) {
-    console.error("Error fetching posts:", error);
-  }
+  const [rows] = await connection.query("SELECT * FROM blogs");
+  const posts = rows;
 
   return (
     <div className="Home">
       <h1 className="text-3xl font-bold text-center">Blog posts</h1>
       <div className="flex flex-col items-center justify-center mt-5 gap-3">
         {
-          posts.map((post)=>{
+          posts?.map((post)=>{
             return(
               <div className="flex flex-col p-2 bg-blue-100 rounded-lg shadow-md gap-2 w-5/7" key={post.id}>
                 <h2 className="text-2xl font-semibold">{post.title}</h2>
