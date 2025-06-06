385a89204949e8e7d4f56dc6e363f44788fd999a
itanga benigne <itgbenny@gmail.com>
2025-06-06 19:34:16 +0200

    Added the deletion functionality by importing the delete button component

--- app/post/[id]/page.js
+++ app/post/[id]/page.js
@@ -1,9 +1,15 @@
-import Axios from 'axios';
+"use server";
+import { redirect } from "next/navigation";
+import { DeleteButton } from "./deleteButton";
 
 export default async function viewPost({ params }) {
   const { id } = await params;
   const response = await fetch(`${process.env.SERVER_URL}/api/posts/${id}`);
   const post = await response.json();
+  if(post.error) {
+    redirect('/'); // Redirect to home page if post not found
+  }
+  
   return (
     <div className="flex flex-col items-center justify-center mt-5 gap-3 min-h-[calc(50vh-90px)]">
       <h1 className="text-3xl font-bold text-center">Post Details</h1>
@@ -11,8 +17,8 @@ export default async function viewPost({ params }) {
         <h2 className="text-2xl font-semibold">{post.title}</h2>
         <p>{post.description}</p>
         <div className=' flex flex-row justify-end gap-2 mt-2'>
+          <DeleteButton id={id} />
           <button className=' py-[10px] px-[20] font-bold text-md text-white rounded-2xl bg-blue-500'>Update</button>
-          <button className=' py-[10px] px-[20] font-bold text-md text-white rounded-2xl bg-red-500'>Delete</button>
         </div>
       </div>
     </div>
